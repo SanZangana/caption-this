ce7fc8d055a7f955f7e7fe44f9fdd68d69081fa9
SanZangana <San9@live.se>
2023-02-21 05:55:52 +0000

    Add bootstrap button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Button from "react-bootstrap/Button";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>HEllo EvErYbOdY!!</h1>
+      <Button variant="info">Info</Button>
     </div>
   );
 }
