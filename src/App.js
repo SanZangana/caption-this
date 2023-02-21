e8ddf6ae663e77053ad9405b634c7404544e1426
SanZangana <San9@live.se>
2023-02-21 13:12:03 +0000

    Add correct react path syntax

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefault";
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -11,7 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => <h1>Home</h1>} />
           <Route exact path="/contact" render={() => <h1>Contact</h1>} />
-          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
           <Route exact path="/signout" render={() => <h1>Sign Out</h1>} />
           <Route render={() => <p>Sorry, page not found!</p>} />
