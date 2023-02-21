34517571851141c4795f9554538e8a3a46809378
SanZangana <San9@live.se>
2023-02-21 08:54:55 +0000

    Add routes and paths to all the pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/contact" render={() => <h1>Contact</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+          <Route exact path="/signout" render={() => <h1>Sign Out</h1>} />
+          <Route render={() => <p>Sorry, page not found!</p>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
