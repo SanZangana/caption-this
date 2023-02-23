210e344cba37314aefee9d45d0fccf92e09a1864
SanZangana <San9@live.se>
2023-02-23 13:59:29 +0000

    Restart whole project because of unfixable error

--- src/api/axiosDefault.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL = "https://caption-this2.herokuapp.com/";
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
-axios.defaults.withCredentials = true;
