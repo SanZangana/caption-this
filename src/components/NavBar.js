e2464b406452052bf60c36f8bc1f8e43d3fd39bd
SanZangana <San9@live.se>
2023-02-24 07:26:18 +0000

    Add classname and activeclassname to signin/signup/home page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,13 +16,26 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <NavLink to="/">
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
               <i className="fas fa-house-user"></i>Home
             </NavLink>
-            <NavLink to="/signin">
+            <NavLink
+              classname={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
               <i class="fas fa-person-walking-arrow-right"></i>Sign In
             </NavLink>
-            <NavLink to="/signup">
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signup"
+            >
               <i className="fas fa-user-plus"></i>Sign Up
             </NavLink>
           </Nav>
