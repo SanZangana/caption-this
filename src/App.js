1f97755ce084a3e0145b0c215172a930bce41130
SanZangana <San9@live.se>
2023-02-24 06:13:08 +0000

    Add classname to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import "./App.module.css";
+import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavBar />
     </div>
   );
