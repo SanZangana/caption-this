c014b46adc9f0189c3642adfd23c81111f582937
SanZangana <San9@live.se>
2023-02-24 06:24:40 +0000

    Add path to links on navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,26 +2,29 @@ import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.svg";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>
-          <img src={logo} alt="logo" height="45" />
-        </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="45" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
+            <NavLink to="/">
               <i className="fas fa-house-user"></i>Home
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink to="/signin">
               <i class="fas fa-person-walking-arrow-right"></i>Sign In
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink to="/signup">
               <i className="fas fa-user-plus"></i>Sign Up
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
