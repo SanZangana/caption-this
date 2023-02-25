662ac5b52a00bf45bebec130b3664b4d9dcc0af9
SanZangana <San9@live.se>
2023-02-25 07:42:42 +0000

    Add useContext hook to the appropriate files2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.svg";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../App";
 
 const NavBar = () => {
+  const currentUser = useContext(CurrentUserContext);
+  const loggedInIcons = <>{currentUser?.username}</>;
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        classname={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i class="fas fa-person-walking-arrow-right"></i>Sign In
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signup"
+      >
+        <i className="fas fa-user-plus"></i>Sign Up
+      </NavLink>
+    </>
+  );
+
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
@@ -24,20 +46,7 @@ const NavBar = () => {
             >
               <i className="fas fa-house-user"></i>Home
             </NavLink>
-            <NavLink
-              classname={styles.NavLink}
-              activeClassName={styles.Active}
-              to="/signin"
-            >
-              <i class="fas fa-person-walking-arrow-right"></i>Sign In
-            </NavLink>
-            <NavLink
-              className={styles.NavLink}
-              activeClassName={styles.Active}
-              to="/signup"
-            >
-              <i className="fas fa-user-plus"></i>Sign Up
-            </NavLink>
+            {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
       </Container>
