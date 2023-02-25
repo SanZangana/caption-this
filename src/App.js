97e2054cbc7f056374a7e2d92f8cdb7dae6e14ca
SanZangana <San9@live.se>
2023-02-25 06:31:01 +0000

    Add CI code to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
-import { Container } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
@@ -14,7 +14,7 @@ function App() {
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route render={() => <h1>Sorry, this page does not exist!</h1>} />
+          <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
     </div>
