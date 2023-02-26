930f28712a2ca735c47418c094ff70ea45ba6fdb
SanZangana <San9@live.se>
2023-02-26 11:56:26 +0000

    Add correct url for axios

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://caption-this-react.herokuapp.com/";
+axios.defaults.baseURL = "https://caption-this2.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
