f08026dc15e0ad6e83e357f09367b3db7a694a8d
SanZangana <San9@live.se>
2023-02-28 08:24:55 +0000

    Add function for searching for a post and loading spinner2

--- src/pages/auth/posts/PostsPage.js
+++ src/pages/auth/posts/PostsPage.js
@@ -1,19 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
+import Posts from "./Posts";
+import Asset from "../../../components/Assets";
+
 import appStyles from "../../../App.module.css";
 import styles from "../../../styles/PostsPage.module.css";
+import { useLocation } from "react-router";
+import { axiosReq } from "../../../api/axiosDefaults";
 
-function PostsPage() {
+import NoResults from "../../../assets/no-results.png";
+
+function PostsPage({ message, filter = "" }) {
+  const [posts, setPosts] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
+        setPosts(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    const timer = setTimeout(() => {
+      fetchPosts();
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+    fetchPosts();
+  }, [filter, query, pathname]);
+
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
-        <p>List of posts here</p>
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search for posts"
+          />
+        </Form>
+
+        {hasLoaded ? (
+          <>
+            {posts.results.length ? (
+              posts.results.map((posts) => (
+                <Posts key={posts.id} {...posts} setPosts={setPosts} />
+              ))
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
         <p>Popular profiles for desktop</p>
