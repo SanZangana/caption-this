753351656fe0aaac828d08fdd96347538c02a457
SanZangana <San9@live.se>
2023-03-01 15:39:00 +0000

    Import comment to PostPage.js

--- src/pages/auth/posts/PostPage.js
+++ src/pages/auth/posts/PostPage.js
@@ -8,6 +8,7 @@ import appStyles from "../../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../../api/axiosDefaults";
 import Posts from "./Posts";
+import Comment from "../comments/Comment";
 
 import CreateCommentForm from "../comments/CreateCommentForm";
 import { useCurrentUser } from "../../../contexts/CurrentUserContext";
@@ -23,11 +24,12 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`),
         ]);
         setPost({ results: [post] });
-        console.log(post);
+        setComments(comments);
       } catch (err) {
         console.log(err);
       }
@@ -52,6 +54,20 @@ function PostPage() {
           ) : comments.results.length ? (
             "Comments"
           ) : null}
+          {comments.results.length ? (
+            comments.results.map((comment) => (
+              <Comment
+                key={comment.id}
+                {...comment}
+                setPost={setPost}
+                setComments={setComments}
+              />
+            ))
+          ) : currentUser ? (
+            <span>No comments yet, be the first to comment!</span>
+          ) : (
+            <span>No comments available</span>
+          )}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
