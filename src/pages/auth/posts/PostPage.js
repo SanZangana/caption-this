149738ca2481b591a2aa49f0ddd0c28d96fae52e
SanZangana <San9@live.se>
2023-03-01 11:02:13 +0000

    Import createcommentformcomponent

--- src/pages/auth/posts/PostPage.js
+++ src/pages/auth/posts/PostPage.js
@@ -9,9 +9,16 @@ import { useParams } from "react-router";
 import { axiosReq } from "../../../api/axiosDefaults";
 import Posts from "./Posts";
 
+import CreateCommentForm from "../comments/CreateCommentForm";
+import { useCurrentUser } from "../../../contexts/CurrentUserContext";
+
 function PostPage() {
   const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
+
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
@@ -33,7 +40,19 @@ function PostPage() {
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
         <Posts {...post.results[0]} setPosts={setPost} postPage />
-        <Container className={appStyles.Content}>Comments</Container>
+        <Container className={appStyles.Content}>
+          {currentUser ? (
+            <CreateCommentForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              post={id}
+              setPost={setPost}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+        </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popular profiles for desktop
