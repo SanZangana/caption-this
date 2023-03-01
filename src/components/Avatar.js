053a91498607bceaac62464d7794d8ee45fa48bb
SanZangana <San9@live.se>
2023-03-01 15:38:22 +0000

    Fix typo for Avatar.js

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -8,7 +8,7 @@ const Avatar = ({ src, height = 45, text }) => {
         className={styles.Avatar}
         src={src}
         height={height}
-        widht={height}
+        width={height}
         alt="avatar"
       />
       {text}
